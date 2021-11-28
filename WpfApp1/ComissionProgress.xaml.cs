ce6dd9357e2a158a1313710bab562c8faa73e3c2
Kiley Carson <kiley.carson@cs.ucalgary.ca>
2021-11-27 18:41:13 -0700

    Changes

---
+++ WpfApp1/ComissionProgress.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class CommissionProgress : Window
+    {
+        public CommissionProgress()
+        {
+        }
+        private void CompleteForm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void AddNewCommission_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+
+    }
+}
---
+++ WpfApp1/NewCommission.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for AddComission.xaml
+    /// </summary>
+    public partial class NewCommission : Window
+    {
+        public NewCommission()
+        {
+        }
+        
+        private void SubmitButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+   
+    }
+}
